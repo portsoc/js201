b3cb9f6f928678d2c30be9ca5a5a00431eaa18e0
ear1grey <rjb@port.ac.uk>
2016-10-05 17:40:17 +0100

    switched to better test format checking for function type and use of strictEquals for better error messages

--- assess.functions.js
+++ assess.functions.js
@@ -1,4 +1,6 @@
-QUnit.module( "Functions" );
+"use strict";
+
+QUnit.module("Functions");
 
 QUnit.test("funcFeelGood",
     function(assert) {
@@ -12,16 +14,19 @@ QUnit.test("funcFeelGood",
 
 QUnit.test("funcAddAll",
     function(assert) {
+
         assert.ok(
-            addAll([1]) == 1,
+            typeof addAll === "function",
             "Create a function addAll that accepts an array as a single parameter.  Assuming an array of numbers is passed, return the result of adding all those numbers."
         );
-        assert.ok( addAll([1,1]) == 2 );
-        assert.ok( addAll([1,1, 1]) == 3 );
-        assert.ok( addAll([1, 2, 3, 4, 5]) == 15 );
-        assert.ok( addAll([1.1, 1.2, 1.3, 1.5]) == 5.1 );
-        assert.ok( addAll([-100,-200,-300]) == -600 );
-        assert.ok( addAll([-200,400]) == 200 );
+
+        assert.strictEqual(addAll([1]), 1);
+        assert.strictEqual(addAll([1, 1]), 2);
+        assert.strictEqual(addAll([1, 1, 1]), 3);
+        assert.strictEqual(addAll([1, 2, 3, 4, 5]), 15);
+        assert.strictEqual(addAll([1.1, 1.2, 1.3, 1.5]), 5.1);
+        assert.strictEqual(addAll([-100, -200, -300]), -600);
+        assert.strictEqual(addAll([-200, 400]), 200);
 
     }
 );
@@ -29,30 +34,36 @@ QUnit.test("funcAddAll",
 
 QUnit.test("funcLarger",
     function(assert) {
+
         assert.ok(
-            larger(1,2) == 2,
+            typeof larger === "function",
             "Create a function called larger that accepts two numbers and returns the larger."
         );
-        assert.ok( larger(1,2) == 2 );
-        assert.ok( larger(1,3) == 3 );
-        assert.ok( larger(2,2) == 2 );
-        assert.ok( larger(3,-5) == 3 );
-        assert.ok( larger(-3,5) == 5 );
+
+        assert.strictEqual(larger(1, 2), 2);
+        assert.strictEqual(larger(1, 2), 2);
+        assert.strictEqual(larger(1, 3), 3);
+        assert.strictEqual(larger(2, 2), 2);
+        assert.strictEqual(larger(3, -5), 3);
+        assert.strictEqual(larger(-3, 5), 5);
     }
 );
 
 
 QUnit.test("funcLargest",
     function(assert) {
+
         assert.ok(
-            largest([1,2]) == 2,
+            typeof largest === "function",
             "Create a function called largest that accepts an array of numbers and returns the largest number."
         );
-        assert.ok( largest([0,1,2]) == 2 );
-        assert.ok( largest([1,1,3]) == 3 );
-        assert.ok( largest([2,2,2]) == 2 );
-        assert.ok( largest([1,2,3,4,5,6,7,8,3,-5]) == 8 );
-        assert.ok( largest([1]) == 1 );
+
+        assert.strictEqual(largest([1, 2]), 2);
+        assert.strictEqual(largest([0, 1, 2]), 2);
+        assert.strictEqual(largest([1, 1, 3]), 3);
+        assert.strictEqual(largest([2, 2, 2]), 2);
+        assert.strictEqual(largest([1, 2, 3, 4, 5, 6, 7, 8, 3, -5]), 8);
+        assert.strictEqual(largest([1]), 1);
     }
 );
 
@@ -60,26 +71,58 @@ QUnit.test("funcLargest",
 QUnit.test("funcCompareArrays",
     function(assert) {
         assert.ok(
-            largest([1,2]) == 2,
+            typeof compare === "function",
             "Create a function called compare that accepts two arrays of numbers and compares the contents.  It should return true if the arrays are identical and false otherwise."
         );
-        assert.ok( compare([1],[1]), "two arrays with 1, should pass" );
-        assert.ok( compare([1,2,3],[1,2,3]), "two more identical arrays, should pass" );
-        assert.ok( compare([4,7,11,17],[4,7,11,17]), "two arrays, for idential elements each, should pass");
-        assert.notOk( compare([1,2],[2,1]), "expect to return false because 1!=2 and 2!=1" );
-        assert.notOk( compare([4,7,11,17],[109]), "totally false" );
+
+        assert.ok(
+            compare([1], [1]),
+            "two arrays with 1, should pass"
+        );
+
+        assert.ok(
+            compare([1, 2, 3], [1, 2, 3]),
+            "two identical arrays, should pass"
+        );
+
+        assert.ok(
+            compare([4, 7, 11, 17], [4, 7, 11, 17]),
+            "two arrays, for idential elements each, should pass"
+        );
+
+        assert.notOk(
+            compare([1, 2], [2, 1]),
+            "expect to return false because 1!=2 and 2!=1"
+        );
+
+        assert.notOk(
+            compare([4, 7, 11, 17], [109]),
+            "totally false"
+        );
     }
 );
 
 
 QUnit.test("funcAddToAll",
     function(assert) {
+
         assert.ok(
-          compare( [2,2], addToAll( [1,1], 1 ) ),
-          "Create a function addToAll that accepts an array and a number as parameters.  Assuming an array of numbers is passed, return the same array with the second parameter added to each number within the array - such that ([1,2,3], 1) should return [2,3,4]."
+            typeof addToAll === "function",
+            "Create a function addToAll that accepts an array and a number as parameters.  Assuming an array of numbers is passed, return the same array with the second parameter added to each number within the array - such that ([1,2,3], 1) should return [2,3,4]."
         );
+
         assert.ok(
-          compare( [55,66,77], addToAll( [11,22,33], 44 ) )
+            compare(
+                [2, 2],
+                addToAll([1, 1], 1)
+            )
+        );
+
+        assert.ok(
+            compare(
+                [55, 66, 77],
+                addToAll([11, 22, 33], 44)
+            )
         );
     }
 );
@@ -87,16 +130,19 @@ QUnit.test("funcAddToAll",
 
 QUnit.test("funcSideEffect",
     function(assert) {
-        rememberThis("Apple");
+
         assert.ok(
-          remembered == "Apple",
-          "Create a function rememberThis that accepts any variable and stores it in a 'global' variable called 'remembered' (which you need to declare).  Each subsequent call to rememberThis should overwrite the value of 'remembered'."
+            typeof rememberThis === "function",
+            "Create a function rememberThis that accepts any variable and stores it in a 'global' variable called 'remembered' (which you need to declare).  Each subsequent call to rememberThis should overwrite the value of 'remembered'."
         );
 
+        rememberThis("Apple");
+        assert.strictEqual(remembered, "Apple");
+
         rememberThis("Banana");
-        assert.ok( remembered == "Banana" );
+        assert.strictEqual(remembered, "Banana");
 
         rememberThis(1337);
-        assert.ok(remembered == 1337);
+        assert.strictEqual(remembered, 1337);
     }
 );
