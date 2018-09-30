654dd1dd8425aa4bba647d441417d466a22d41ea
Lagicrus <Lagicrus@users.noreply.github.com>
2018-09-30 15:08:23 +0100

    main

--- index.js
+++ index.js
@@ -10,23 +10,96 @@
  */
 
 'use strict';
+var remembered;
 
 function id() {
-    return "Replace This With Your Student ID";
+    return "UP857256";
     // e.g. return "UP654321";
 }
 
 function fn() {
-    return "Replace This With Your First Name";
+    return "Sam";
 }
 
 function sn() {
-    return "Replace This With Your Surname";
+    return "Riches";
 }
 
-function example() {
+function add(first_val, second_val) {
     // replace this example with
     // your first function then
     // add more below as necessary.
-    return;
+    return first_val + second_val;
+}
+
+function subtract(first_val, second_val) {
+    return first_val - second_val
+}
+
+function checkObject(object) {
+    object.checked = true;
+    return object
+}
+
+function checkObjectInside(object) {
+    if (object.data != null) {
+        object.data.checked = true;
+    }
+    return object
+}
+
+function arraySet(array, number, value) {
+    if (number in array) {
+        array[number] = value;
+    }
+    return array
+}
+
+function addAll(array) {
+    let total = 0;
+    for (const value in array) {
+        total += parseFloat(array[value])
+    }
+    return total
+
+}
+
+function larger(val_1, val_2) {
+    return Math.max(val_1, val_2)
+}
+
+function largest(array){
+    return Math.max(...array)
+}
+
+function compare(array_1, array_2) {
+    console.log(array_1, array_2);
+    for (const value in array_1) {
+        if (!(value in array_2)) {
+            return false;
+        }
+        if (!(array_1[value] === array_2[value])) {
+            return false;
+        }
+    }
+    return true
+}
+
+function addToAll(array, number) {
+    for (const value in array) {
+        array[value] += number;
+    }
+    return array
+}
+
+function rememberThis(variable){
+    remembered = variable
+}
+
+function nArray(n){
+    let array = [];
+    for (var i = 0; i < n; i++) {
+        array.push(i);
+    }
+    return array
 }
